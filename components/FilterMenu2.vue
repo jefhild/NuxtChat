824893f16b3c167d04abb2e71e579b85203cdcee
Marrouche <ramboxxrhino2004@gmail.com>
2025-05-06 14:31:28 +0200

    filter style changed

--- components/FilterMenu2.vue
+++ components/FilterMenu2.vue
@@ -3,112 +3,105 @@
     <v-col cols="auto">
       <v-menu v-model="menu" :close-on-content-click="false" location="end">
         <template v-slot:activator="{ props }">
-          <v-btn variant="text" color="green" v-bind="props">
-            Filter
-
-            <v-badge v-if="
-                selectedGender !== null ||
-                selectedAge[0] !== 18 ||
-                selectedAge[1] !== 100
-              " color="red" dot overlap class="exclamation-badge">
-            </v-badge>
+          <v-btn variant="tonal" color="primary" v-bind="props" class="rounded-lg">
+            <v-icon start>mdi-filter-variant</v-icon>
+            Filters
+            <v-badge v-if="selectedGender !== null || selectedAge[0] !== 18 || selectedAge[1] !== 100" color="red" dot
+              overlap class="ml-2" />
           </v-btn>
         </template>
 
-        <v-card min-width="350">
-          <v-list>
-            <v-list-item v-if="userProfile" :prepend-avatar="userProfile.avatar_url" :subtitle="userProfile.tagline"
-              :title="userProfile.displayname">
-            </v-list-item>
-          </v-list>
-          <v-list>
-            <v-list-item class="text-center">
-              <v-btn @click="handleToggleUsers" class="mb-3">
-                {{ showAIUsers ? "Human Users" : "AI Users" }}
-              </v-btn>
-            </v-list-item>
-          </v-list>
+        <v-card class="pa-4" width="400" rounded="lg" elevation="4">
+          <!-- User Info -->
+          <v-row class="mb-4 mt-2" align="center" no-gutters>
+            <div class="d-flex flex-column justify-center">
+              <span class="text-body-1 font-weight-medium">{{ userProfile.displayname }}</span>
+              <span class="text-caption text-grey">{{ userProfile.tagline }}</span>
+            </div>
+          </v-row>
 
-          <v-list>
-            <v-list-item>
-              <v-select v-model="selectedGender" :items="genders" item-title="text" item-value="value" label="Gender"
-                variant="underlined" @change="applyFilters"></v-select>
-            </v-list-item>
-          </v-list>
+          <v-divider class="my-2" />
 
-          <v-list>
-            <v-list-item>
-              <div class="slider-label mb-2">Age Range</div>
-              <v-range-slider v-model="selectedAge" :min="18" :max="80" :step="1" hide-details color="primary"
-                track-color="grey lighten-2" @change="applyFilters">
-                <!-- Display value on the left (prepend slot) -->
-                <template v-slot:prepend>
-                  <span style="
-                      width: 70px;
-                      display: inline-block;
-                      text-align: center;
-                    ">
-                    {{ selectedAge[0] }}
-                  </span>
+          <!-- AI Toggle  -->
+          <v-list-subheader>
+            <v-icon size="18" class="mr-1">mdi-robot</v-icon> Show AI Users
+          </v-list-subheader>
+          <v-switch :model-value="showAIUsers" @update:model-value="handleToggleUsers" color="primary" hide-details
+            inset class="mr-10" style="margin-top: -4px" />
+
+          <!-- Gender -->
+          <v-list-subheader>
+            <v-icon size="18" class="mr-1">mdi-gender-male-female</v-icon> Gender
+          </v-list-subheader>
+          <v-select v-model="selectedGender" :items="genders" item-title="text" item-value="value" density="compact"
+            variant="outlined" class="mb-4" @change="applyFilters" />
+
+          <!-- Age Range -->
+          <v-list-subheader>
+            <v-icon size="18" class="mr-1">mdi-calendar-range</v-icon> Age Range
+          </v-list-subheader>
+          <v-range-slider v-model="selectedAge" :min="18" :max="80" step="1" class="mb-4" color="deep-purple"
+            @change="applyFilters">
+            <template #prepend>
+              <v-chip size="small" color="grey-lighten-1" text-color="black">{{ selectedAge[0] }}</v-chip>
+            </template>
+            <template #append>
+              <v-chip size="small" color="grey-lighten-1" text-color="black">{{ selectedAge[1] }}</v-chip>
+            </template>
+          </v-range-slider>
+
+          <!-- Anonymity -->
+          <v-list-subheader>
+            <v-icon size="18" class="mr-1">mdi-incognito</v-icon> Anonymity
+          </v-list-subheader>
+          <v-select v-model="selectedAnonymous" :items="anonymity" item-title="text" item-value="value"
+            density="compact" variant="outlined" class="mb-4" @change="applyFilters" />
+
+          <!-- Interests -->
+          <v-list-subheader>
+            <v-icon size="18" class="mr-1">mdi-heart-multiple</v-icon> Interests
+          </v-list-subheader>
+          <v-select v-model="selectedInterests" :items="interests" item-title="name" item-value="id"
+            label="Choose interests" multiple chips closable-chips variant="outlined" density="compact" class="mb-4"
+            @change="applyFilters">
+            <!-- Selection Chip -->
+            <template #selection="{ item }">
+              <v-chip :color="item.raw.color" size="small" class="ma-1">
+                <v-icon start size="small">{{ item.raw.icon }}</v-icon>
+                <span class="mr-1">{{ item.raw.emoji }}</span>
+                {{ item.raw.name }}
+              </v-chip>
+            </template>
+
+            <!-- Dropdown Item List -->
+            <template #item="{ item, props }">
+              <v-list-item v-bind="props">
+                <template #prepend>
+                  <v-icon size="small" :color="item.raw.color" class="mr-2">{{ item.raw.icon }}</v-icon>
+                  <span class="mr-2">{{ item.raw.emoji }}</span>
                 </template>
-                <!-- Display value on the right (append slot) -->
-                <template v-slot:append>
-                  <span style="
-                      width: 70px;
-                      display: inline-block;
-                      text-align: center;
-                    ">
-                    {{ selectedAge[1] }}
-                  </span>
-                </template>
-              </v-range-slider>
-            </v-list-item>
-          </v-list>
+              </v-list-item>
+            </template>
+          </v-select>
 
-          <v-list>
-            <v-list-item>
-              <v-select v-model="selectedAnonymous" :items="anonymity" label="Anonymity" item-title="text"
-                item-value="value" variant="underlined" @change="applyFilters"></v-select>
-            </v-list-item>
-          </v-list>
-
-          <!-- interests -->
-          <v-list>
-            <v-list-item>
-              <v-select v-model="selectedInterests" :items="interests" item-title="name" item-value="id"
-                label="Interests" multiple chips closable-chips variant="underlined" @change="applyFilters">
-                <template #selection="{ item, index }">
-                  <v-chip v-if="item" :key="index" :color="item.raw.color" size="small" class="ma-1">
-                    <v-icon left size="small">{{ item.raw.icon }}</v-icon>
-                    {{ item.raw.name }}
-                  </v-chip>
-                </template>
-
-                <template #item="{ item, props }">
-                  <v-list-item v-bind="props">
-                    <template #prepend>
-                      <v-icon size="small" :color="item.raw.color">{{ item.raw.icon }}</v-icon>
-                    </template>
-                  </v-list-item>
-                </template>
-              </v-select>
-            </v-list-item>
-          </v-list>
-
+          <!-- Actions -->
+          <v-divider class="my-3" />
           <v-card-actions>
-            <v-spacer></v-spacer>
-
-            <v-btn variant="text" @click="menu = false"> Cancel </v-btn>
-            <v-btn color="primary" variant="text" @click="saveFilters">
-              Save
-            </v-btn>
+            <v-spacer />
+            <v-btn variant="text" @click="menu = false">Cancel</v-btn>
+            <v-btn color="primary" variant="flat" @click="saveFilters">Apply</v-btn>
           </v-card-actions>
         </v-card>
       </v-menu>
     </v-col>
-    <v-col class="text-caption text-medium-emphasis mt-2">{{ rowCount }} users online</v-col>
+
+    <v-col class="text-caption text-medium-emphasis mt-2">
+      {{ rowCount }} users online
+    </v-col>
   </v-row>
 </template>
+
+
 
 <script setup>
 
@@ -138,6 +131,8 @@ const props = defineProps({
   },
   showAIUsers: Boolean, // Accept the current state as a prop
 });
+
+const localShowAIUsers = ref(props.showAIUsers);
 
 const emit = defineEmits(["filter-changed","toggle-users"]);
 const genders = [
@@ -189,6 +184,7 @@ onMounted(async () =>
   const {data} = await getInterests();
   interests.value = data;
   selectedInterests.value = interests.value.map((interest) => interest.id); // Select all interests by default
+  console.log("userprofile", props.userProfile);  
 });
 </script>
 
