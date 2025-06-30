7fc1af7b7b4adf19dbaade19af0f99228eec6bb6
Jeffrey <jefhild@hotmail.com>
2025-06-30 09:15:47 +0200

    NavBar Auth Fix

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -15,123 +15,143 @@
         <nav role="navigation" aria-label="Main Navigation">
           <!-- <v-row class="d-none d-md-flex" align="center"> -->
           <div class="d-none d-md-flex flex-column align-end">
-            <v-row align="center">
-              <NuxtLink
-                :to="localPath('/articles')"
-                class="v-btn text-button navItem mr-3"
-                exact
+            <v-row align="center" no-gutters>
+              <v-col>
+                <NuxtLink
+                  :to="localPath('/articles')"
+                  class="v-btn text-button navItem mr-3"
+                  exact
+                >
+                  <!-- <v-icon start>mdi-post-outline</v-icon> -->
+                  {{ $t("components.navbar.blog") }}
+                </NuxtLink></v-col
               >
-                <!-- <v-icon start>mdi-post-outline</v-icon> -->
-                {{ $t("components.navbar.blog") }}
-              </NuxtLink>
+              <v-col>
+                <NuxtLink
+                  :to="localPath('/about')"
+                  class="v-btn text-button navItem mr-4"
+                  exact
+                >
+                  <!-- <v-icon start>mdi-account-group</v-icon> -->
+                  {{ $t("components.navbar.aboutus") }}
+                </NuxtLink></v-col
+              >
 
-              <NuxtLink
-                :to="localPath('/about')"
-                class="v-btn text-button navItem mr-4"
-                exact
+              <v-col>
+                <NuxtLink
+                  :to="localPath('/profiles')"
+                  class="v-btn text-button navItem mr-4"
+                  exact
+                >
+                  <!-- <v-icon start>mdi-monitor-account</v-icon> -->
+                  {{ $t("components.navbar.free-chat") }}
+                </NuxtLink></v-col
               >
-                <!-- <v-icon start>mdi-account-group</v-icon> -->
-                {{ $t("components.navbar.aboutus") }}
-              </NuxtLink>
+              <v-col v-if="isAuthenticated">
+                <NuxtLink
+                  :to="localPath('/chat')"
+                  class="v-btn text-button navItem mr-4"
+                  exact
+                >
+                  <!-- <v-icon start>mdi-monitor-account</v-icon> -->
+                  {{ $t("components.navbar.chat") }}
+                </NuxtLink></v-col
+              >
 
-              <NuxtLink
-                :to="localPath('/profiles')"
-                class="v-btn text-button navItem mr-4"
-                exact
-              >
-                <!-- <v-icon start>mdi-monitor-account</v-icon> -->
-                {{ $t("components.navbar.free-chat") }}
-              </NuxtLink>
+              <v-col v-if="isAuthenticated">
+                <NuxtLink
+                  :to="localPath('/settings')"
+                  class="v-btn text-button navItem mr-3"
+                  exact
+                >
+                  <v-icon start>mdi-cog</v-icon>
+                  {{ $t("components.navbar.settings") }}
+                </NuxtLink>
+              </v-col>
 
-              <NuxtLink
-                v-if="isAuthenticated"
-                :to="localPath('/settings')"
-                class="v-btn text-button navItem mr-3"
-                exact
-              >
-                <v-icon start>mdi-cog</v-icon>
-                {{ $t("components.navbar.settings") }}
-              </NuxtLink>
-
-              <v-btn
-                v-if="isAuthenticated"
-                @click="showLogoutDialog"
-                variant="text"
-              >
-                <v-icon start>mdi-logout</v-icon>
-                {{ $t("components.navbar.logout") }}
-              </v-btn>
-
-              <v-btn
-                v-else
-                :to="localPath('/signin')"
-                variant="text"
-                class="navItem mr-4"
-              >
-                <v-icon start>mdi-login</v-icon>
-                {{ $t("components.navbar.signin") }}
-              </v-btn>
+              <v-col>
+                <v-btn
+                  v-if="isAuthenticated"
+                  @click="showLogoutDialog"
+                  variant="text"
+                >
+                  <v-icon start>mdi-logout</v-icon>
+                  {{ $t("components.navbar.logout") }}
+                </v-btn>
+                <v-btn
+                  v-else
+                  :to="localPath('/signin')"
+                  variant="text"
+                  class="navItem mr-4"
+                >
+                  <v-icon start>mdi-login</v-icon>
+                  {{ $t("components.navbar.signin") }}
+                </v-btn>
+              </v-col>
             </v-row>
 
-            <v-row><LanguageSwitcher /></v-row>
+            <v-row no-gutters><LanguageSwitcher /></v-row>
           </div>
         </nav>
 
         <!-- Mobile menu -->
-        <div class="d-flex d-md-none">
-          <v-menu>
-            <template #activator="{ props }">
-              <v-app-bar-nav-icon v-bind="props" />
-            </template>
 
-            <v-list aria-label="Mobile Navigation">
-              <v-list-item :to="localPath('/articles')" link>
-                <v-list-item-title>{{
-                  $t("components.navbar.blog")
-                }}</v-list-item-title>
-              </v-list-item>
+        <v-row no-gutters>
+          <div class="d-flex d-md-none">
+            <v-menu>
+              <template #activator="{ props }">
+                <v-app-bar-nav-icon v-bind="props" />
+              </template>
 
-              <v-list-item :to="localPath('/about')" link>
-                <v-list-item-title>{{
-                  $t("components.navbar.aboutus")
-                }}</v-list-item-title>
-              </v-list-item>
+              <v-list aria-label="Mobile Navigation">
+                <v-list-item :to="localPath('/articles')" link>
+                  <v-list-item-title>{{
+                    $t("components.navbar.blog")
+                  }}</v-list-item-title>
+                </v-list-item>
 
-              <v-list-item :to="localPath('/profiles')" link>
-                <v-list-item-title>{{
-                  $t("components.navbar.free-chat")
-                }}</v-list-item-title>
-              </v-list-item>
+                <v-list-item :to="localPath('/about')" link>
+                  <v-list-item-title>{{
+                    $t("components.navbar.aboutus")
+                  }}</v-list-item-title>
+                </v-list-item>
 
-              <v-list-item
-                v-if="isAuthenticated"
-                :to="localPath('/settings')"
-                link
-              >
-                <v-list-item-title>{{
-                  $t("components.navbar.settings")
-                }}</v-list-item-title>
-              </v-list-item>
+                <v-list-item :to="localPath('/profiles')" link>
+                  <v-list-item-title>{{
+                    $t("components.navbar.free-chat")
+                  }}</v-list-item-title>
+                </v-list-item>
 
-              <v-list-item v-if="isAuthenticated" @click="showLogoutDialog">
-                <v-list-item-title>{{
-                  $t("components.navbar.logout")
-                }}</v-list-item-title>
-              </v-list-item>
+                <v-list-item
+                  v-if="isAuthenticated"
+                  :to="localPath('/settings')"
+                  link
+                >
+                  <v-list-item-title>{{
+                    $t("components.navbar.settings")
+                  }}</v-list-item-title>
+                </v-list-item>
 
-              <v-list-item
-                v-else
-                :to="localPath('/signin')"
-                prepend-icon="mdi-login"
-                link
-              >
-                <v-list-item-title>{{
-                  $t("components.navbar.signin")
-                }}</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-        </div>
+                <v-list-item v-if="isAuthenticated" @click="showLogoutDialog">
+                  <v-list-item-title>{{
+                    $t("components.navbar.logout")
+                  }}</v-list-item-title>
+                </v-list-item>
+
+                <v-list-item
+                  v-else
+                  :to="localPath('/signin')"
+                  prepend-icon="mdi-login"
+                  link
+                >
+                  <v-list-item-title>{{
+                    $t("components.navbar.signin")
+                  }}</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </div>
+        </v-row>
       </template>
     </v-app-bar>
   </nav>
